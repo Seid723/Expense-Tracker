c578cbb49a2971cf40491e31ea26dcc709f9f701
Seid959 <isgenderliseid614@gmail.com>
2026-02-28 15:46:05 +0100

    first upload~

---
+++ Expense/Controllers/ExpenseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expense.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
