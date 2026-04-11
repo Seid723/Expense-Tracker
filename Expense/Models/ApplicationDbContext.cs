141bc8251f20e30edc500d407aa84be443b4bd42
Seid959 <isgenderliseid614@gmail.com>
2026-04-11 22:18:59 +0200

    Updated project

--- Expense/Models/ApplicationDbContext.cs
+++ Expense/Models/ApplicationDbContext.cs
@@ -1,4 +1,11 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
 namespace Expense.Models
 {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) { }
+
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
 }
